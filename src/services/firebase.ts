8861fd193b820180ae36803682d0595afa3e1437
Moisés <moisesnetored@gmail.com>
2022-02-03 12:19:55 -0400

    feat: Added environment variables to the project, tweaks in Firebase to have compatibility with is v8

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/database';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_IDD
+};
+
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+
+// export const auth = firebase.auth();
+// export const database = firebase.database();
+
