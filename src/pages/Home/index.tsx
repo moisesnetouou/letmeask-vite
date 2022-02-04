b46df20ec680464a292ad8dbf4cbc8e49b19fa89
Moisés <moisesnetored@gmail.com>
2022-02-04 11:35:01 -0400

    feat: Flow entering a functional room.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,14 @@ import { Button } from '../../components/Button';
 import { useAuth } from '../../hooks/useAuth';
 
 import './styles.scss';
+import { FormEvent, useState } from 'react';
+import { database } from '../../services/firebase';
 
 export function Home(){
   const navigate = useNavigate();
   const { signInWithGoogle, user } = useAuth();
+
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
     if(!user){
@@ -18,6 +22,23 @@ export function Home(){
     }
 
     navigate('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()){
+      alert('Room does not exists.')
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`)
   }
 
   return(
@@ -38,8 +59,13 @@ export function Home(){
           </button>
           <div className="separator">ou entre em uma sala</div>
 
-          <form>
-            <input type="text" placeholder='Digite o código da sala' />
+          <form onSubmit={handleJoinRoom}>
+            <input 
+              type="text" 
+              placeholder='Digite o código da sala' 
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
