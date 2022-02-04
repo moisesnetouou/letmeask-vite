2965429d259246cf5c69021b5df8aef2f87c14d0
Moisés <moisesnetored@gmail.com>
2022-02-04 09:57:27 -0400

    feat: Authentication state recovered, if user logged in before, wven with refresh it keeps the data.

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useContext, useState} from 'react';
+import {createContext, ReactNode, useContext, useEffect, useState} from 'react';
 
 import {auth, firebase} from '../services/firebase';
 import {AuthContextType, User } from './@types/auth';
@@ -8,6 +8,25 @@ export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({children}: {children: ReactNode}){
   const [user, setUser] = useState<User>();
+
+  useEffect(()=> {
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        const {displayName, photoURL, uid} = user;
+
+        if(!displayName || !photoURL){
+          throw new Error('Missing information from Google Account');
+        }
+
+        setUser({
+          id: uid, 
+          name: displayName, 
+          avatar: photoURL
+        })
+      }
+    });
+  }, [])
+
 
   async function signInWithGoogle(){
     const provider = new firebase.auth.GoogleAuthProvider();
