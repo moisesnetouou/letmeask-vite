21bbb98cb14e7a94e2bbbf3ae2ef5f433475acd2
Moisés <moisesnetored@gmail.com>
2022-08-09 10:25:01 -0400

    feat: Navigation between pages adjusted.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import {useNavigate} from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -6,6 +7,12 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function Home(){
+  const navigate = useNavigate();
+
+  function navigateToNewRoom(){
+    navigate('rooms/new');
+  }
+
   return(
     <div id="page-auth">
       <aside>
@@ -17,7 +24,7 @@ export function Home(){
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button className="create-room" onClick={()=> navigateToNewRoom()}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
@@ -27,7 +28,7 @@ export function NewRoom(){
           </form>
 
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
